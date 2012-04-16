5147d06c69f0eca3b5896239dee47b431b069eec
Gautham Nookala <gauthamnkl@gmail.com>
2012-04-16 13:53:20 -0700

    Added Registration Form

---
+++ bookmarks/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+class RegistrationForm(forms.Form):
+	username = forms.CharField(
+		label=u'Username', 
+		max_length=30
+	)
+	email = forms.CharField(label=u'Email')
+	password1 = forms.CharField(
+		label=u'Password',
+		widget=forms.PasswordInput()
+	)
+	password2 = forms.CharField(
+		label=u'Confirm Password',
+		widget=forms.PasswordInput()
+	)
