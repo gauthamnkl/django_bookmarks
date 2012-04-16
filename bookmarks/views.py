bb0dfbeb08eeaf7bd679a47678e85ee2af4a26dc
Gautham Nookala <gauthamnkl@gmail.com>
2012-04-16 13:16:28 -0700

    Modified views

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -3,20 +3,21 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
+from django.template import RequestContext
 
 def main_page(request):
 	return render_to_response(
-		'main_page.html',
-		{'user': request.user}
+		'main_page.html', RequestContext(request)
 	)
 
 def user_page(request, username):
 	user = get_object_or_404(User, username=username)
 	bookmarks = user.bookmark_set.all()
-	return render_to_response(
-		'user_page.html',
-		{'username': username, 'bookmarks': bookmarks}
-	)
+	variables = RequestContext(request, {
+	'username': username,
+	'bookmarks': bookmarks
+})
+	return render_to_response('user_page.html', variables)
 
 def logout_page(request):
 	logout(request)
--- urls.py
+++ urls.py
@@ -1,8 +1,16 @@
+import os
 from django.conf.urls.defaults import *
 from bookmarks.views import *
+
+site_media = os.path.join(
+	os.path.dirname(__file__), 'site_media'
+)
+
 urlpatterns = patterns('', 
 	(r'^$', main_page),
 	(r'^user/(\w+)/$', user_page),
 	(r'^login/$', 'django.contrib.auth.views.login'),
 	(r'^logout/$', logout_page),
+	(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+		{'document_root': site_media}),
 )
