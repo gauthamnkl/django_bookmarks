6232ee43b555acf6168122a4bf7e8d43be80fa63
Gautham Nookala <gauthamnkl@gmail.com>
2012-04-16 00:35:08 -0700

    First commit

---
+++ bookmarks/views.py
@@ -0,0 +1,26 @@
+# Create your views here.
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
+
+def main_page(request):
+	return render_to_response(
+		'main_page.html',
+		{'user': request.user}
+	)
+
+def user_page(request, username):
+	try:
+		user = User.objects.get(username = username)
+	except User.DoesNotExist:
+		raise Http404(u'Requested user not found.')
+	bookmarks = user.bookmark_set.all()
+	return render_to_response(
+		'user_page.html',
+		{'username': username, 'bookmarks': bookmarks}
+	)
+
+def logout_page(request):
+	logout(request)
+	return HttpResponseRedirect('/')
