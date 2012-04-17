948e815ecf15f0500a987cc717e0da2f0ba9158b
Gautham Nookala <gauthamnkl@gmail.com>
2012-04-17 12:52:54 -0700

    Modified urls.py

--- urls.py
+++ urls.py
@@ -7,14 +7,22 @@ site_media = os.path.join(
 	os.path.dirname(__file__), 'site_media'
 )
 
-urlpatterns = patterns('', 
-	(r'^$', main_page),
-	(r'^user/(\w+)/$', user_page),
-	(r'^login/$', 'django.contrib.auth.views.login'),
-	(r'^logout/$', logout_page),
-	(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
-		{'document_root': site_media}),
-	(r'register/$', register_page),
-	(r'register/success/$', direct_to_template, {'template': 'registration/register_success.html'}),
-	(r'^save/$', bookmark_save_page),
+urlpatterns = patterns('',
+  # Browsing
+  (r'^$', main_page),
+  (r'^user/(\w+)/$', user_page),
+
+  # Session management
+  (r'^login/$', 'django.contrib.auth.views.login'),
+  (r'^logout/$', logout_page),
+  (r'^register/$', register_page),
+  (r'^register/success/$', direct_to_template,
+     {'template': 'registration/register_success.html'}),
+
+  # Account management
+  (r'^save/$', bookmark_save_page),
+
+  # Site media
+  (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+     {'document_root': site_media}),
 )
