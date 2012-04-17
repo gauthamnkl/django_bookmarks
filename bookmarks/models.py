c7de6caecc92c67b5dd86bc8d3194873c7561601
Gautham Nookala <gauthamnkl@gmail.com>
2012-04-16 23:20:41 -0700

    Added Tag model

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -5,10 +5,21 @@ from django.contrib.auth.models import User
 class Link(models.Model):
 	url = models.URLField(unique = True)
 
+	def __unicode__(self):
+		return self.url
+
 class Bookmark(models.Model):
 	title = models.CharField(max_length = 200)
 	user = models.ForeignKey(User)
 	link = models.ForeignKey(Link)
 
+	def __unicode__(self):
+		return u'%s, %s, %s' % (self.title, self.link.url, self.user.username)
 
+class Tag(models.Model):
+	name = models.CharField(max_length = 64, unique = True)
+	bookmark = models.ManyToManyField(Bookmark)
+
+	def __unicode__(self):
+		return self.name
 
